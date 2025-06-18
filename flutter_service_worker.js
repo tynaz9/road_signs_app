a000112da6018f780d86a113970e5bfe2f5a5ae7
Tinashe Malvin Zuze <23p31a05r2@acet.ac.in>
2025-06-18 17:21:07 +0530

    Fix base href and assets

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ef1516f9c236681aa11666c03cd76eb5
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "3c2d665e3794664409f7eac654200526",
+"flutter_bootstrap.js": "a3d3f1aeccc36811f5fe2f38c6fc3c0a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
